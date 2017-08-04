340d2fe28cbc12d85f8655e95197a25b7559dab3
Jushita Rahman <jushitaa@gmail.com>
2017-08-04 13:37:54 -0400

    objects created

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+__author__ = "jushitaa"
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+
---
+++ src/run.py
@@ -0,0 +1,4 @@
+from src.app import Flask
+
+if __name__ == "__main__":
+    app.run(debug=app.config['DEBUG'], port=4990)
